bbaf788309554da1f70d1f2c9df9ba886c50f412
ruben <rubenlorenzo85@yahoo.es>
2020-09-24 09:41:19 +0200

    add button content to the screen

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 function clickNumber(num){
+    addScreen(num);
+}
 
+function addScreen(btnContent){
+    let screen=document.getElementById("screen");
+    screen.value+=btnContent;
 }
 
 function clickOperator(op){
-    
+    addScreen(op);
 }
